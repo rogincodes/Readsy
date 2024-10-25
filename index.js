e64a2e22115337f4c1f505c7813432c76f3fcb4a
rogincodes <cartallarogin@gmail.com>
2024-10-25 20:23:03 +0800

    added functionality on login and signup

--- index.js
+++ index.js
@@ -2,25 +2,43 @@ import express, { query } from "express";
 import axios, { all } from "axios";
 import bodyParser from "body-parser";
 import pg from "pg";
+import bcrypt from "bcrypt";
+import passport from "passport";
+import { Strategy } from "passport-local";
+import session from "express-session";
+import env from "dotenv";
 
 const app = express();
 const port = 3000;
+const saltRounds = 10;
+env.config();
 
-//Middleware
+app.use(session({
+  secret: process.env.SESSION_SECRET, // Add a secret key here
+  resave: false, // Optional, depending on your requirements
+  saveUninitialized: true, // Optional, depending on your requirements
+  cookie: { secure: false } // Adjust this based on your environment (use true in production with HTTPS)
+}));
+
+// Middleware
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Connect to database
+app.use(passport.initialize());
+app.use(passport.session()); 
+
+
+// Connect to database
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "bookshelf",
-  password: "PostgresinG",
-  port: 5432,
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
 });
 db.connect();
 
-//Variables
+// Variables
 let featured = [];
 let random = [];
 let latest = [];
@@ -32,6 +50,9 @@ let allGenres = [];
 let selectedGenre = "";
 let booksByAuthor = [];
 let userQuery = "";
+let userAlreadyExist = "";
+let userNotFound = "";
+let incorrectPsswrd = "";
 
 async function getFeatured() {
   try {
@@ -106,32 +127,71 @@ async function getBooksByAuthor() {
   }
 }
 
-//GET to sign-up page
+// GET to sign-up page
 app.get("/signup", (req, res) => {
-  res.render("signup.ejs");
+  res.render("signup.ejs", {
+    alreadyExist: userAlreadyExist
+  });
 });
 
-//GET to login page
+// GET to login page
 app.get("/login", (req, res) => {
-  res.render("login.ejs");
+  res.render("login.ejs", {
+    notFound: userNotFound,
+    wrongPsswrd: incorrectPsswrd
+  });
 });
 
-//GET to home page
+// GET to home page
 app.get("/", async (req, res) => {
-  await getAllBooks();
-  await randomBook();
-  await getFeatured();
-  await getLatest();
-  res.render("index.ejs",
-    { 
-      lucky: random,
-      featuredBooks: featured,
-      latestBooks: latest,
-      isbnError: invalidISBN,
-    });
+  console.log(req.user);
+  if (req.isAuthenticated()) {
+    await getAllBooks();
+    await randomBook();
+    await getFeatured();
+    await getLatest();
+    res.render("index.ejs",
+      { 
+        lucky: random,
+        featuredBooks: featured,
+        latestBooks: latest,
+        isbnError: invalidISBN,
+      });
+  } else {
+    res.redirect("/login");
+  }
 });
 
-//POST new book journal
+// GET to about page
+app.get("/about.ejs", async (req, res) => {
+  res.render("about.ejs");
+});
+
+// GET to contact page
+app.get("/contact.ejs", async (req, res) => {
+  res.render("contact.ejs");
+});
+
+// GET log out
+app.get("/logout", (req, res) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/login");
+  })
+})
+
+// GET to new book page
+app.get("/new.ejs", async (req, res) => {
+  res.render("new.ejs",
+    { 
+      isbnError: invalidISBN,
+    }
+  );
+});
+
+// POST new book journal
 app.post("/add", async (req, res) => {
   const isbn = req.body.isbn;
   const title = req.body.title.trim();
@@ -142,10 +202,10 @@ app.post("/add", async (req, res) => {
   const review = req.body.review;
   const notes = req.body.notes;
   try {
-    //GET book cover from API
+    // GET book cover from API
     const result = await axios.get(`https://bookcover.longitood.com/bookcover/${isbn}`);
     const img_URL = result.data.url;
-    //INSERT data
+    // INSERT data
     try {
       await db.query("INSERT INTO books (isbn, title, author, genre, img_URL, date_Read, rating, review, notes)VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);", [ isbn, title, author, genre, img_URL, date, rating, review, notes ]);
       invalidISBN = "";
@@ -160,7 +220,7 @@ app.post("/add", async (req, res) => {
   }
 });
 
-//GET to specific book journal by id
+// GET to specific book journal by id
 app.get("/journal/:id", async (req, res) => {
   const rawId = req.params.id.trim();
   const id = parseInt(rawId, 10); 
@@ -183,7 +243,7 @@ app.get("/journal/:id", async (req, res) => {
   } 
 });
 
-//GET to edit page
+// GET to edit page
 app.get("/edit.ejs", async (req, res) => {
   const dateTimeString = specifiedBook.date_read;
   let dateOnly;
@@ -203,7 +263,7 @@ app.get("/edit.ejs", async (req, res) => {
   );
 });
 
-//UPDATE book journal
+// UPDATE book journal
 app.post("/modify", async (req, res) => {
   const isbn = req.body.isbn;
   const title = req.body.title.trim();
@@ -215,10 +275,10 @@ app.post("/modify", async (req, res) => {
   const notes = req.body.notes;
   const id = specifiedBook.id
   try {
-    //GET book cover from API
+    // GET book cover from API
     const result = await axios.get(`https://bookcover.longitood.com/bookcover/${isbn}`);
     const img_URL = result.data.url;
-    //UPDATE data
+    // UPDATE data
     try {
       await db.query("UPDATE books set isbn = $1, title = $2, author = $3, genre = $4, img_URL = $5, date_Read = $6, rating = $7, review = $8, notes = $9 WHERE id = $10;", [ isbn, title, author, genre, img_URL, date, rating, review, notes, id ]);
       invalidISBN = "";
@@ -233,7 +293,7 @@ app.post("/modify", async (req, res) => {
   }
 });
 
-//DELETE a book journal
+// DELETE a book journal
 app.post("/delete/:id", async (req, res) => {
   const id = specifiedBook.id;
   try {
@@ -245,7 +305,7 @@ app.post("/delete/:id", async (req, res) => {
   }
 });
 
-//GET to books page
+// GET to books page
 app.get("/books.ejs", async (req, res) => {
   await getAllBooks();
   await getGenres();
@@ -260,7 +320,7 @@ app.get("/books.ejs", async (req, res) => {
   );
 });
 
-//POST sort books
+// POST sort books
 app.post("/sort", async (req, res) => {
   sortOption = req.body.sort;
   await sortBooks();
@@ -271,7 +331,7 @@ app.post("/sort", async (req, res) => {
   });
 });
 
-//POST sort by genre
+// POST sort by genre
 app.post("/genre", async (req, res) => {
   selectedGenre = req.body.genre;
   await getAllBooks();
@@ -284,7 +344,7 @@ app.post("/genre", async (req, res) => {
   });
 });
 
-//GET search book
+// GET search book
 app.get('/search', async (req, res) => {
   await getAllBooks();
   await getBooksByGenre();
@@ -299,23 +359,88 @@ app.get('/search', async (req, res) => {
   });
 });
 
-//GET to about page
-app.get("/about.ejs", async (req, res) => {
-  res.render("about.ejs");
-});
+// POST login authentication
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login"
+  })
+);
 
-//GET to contact page
-app.get("/contact.ejs", async (req, res) => {
-  res.render("contact.ejs");
-});
+// POST register account
+app.post("/register", async (req, res) => {
+    const email = req.body.username;
+    const password = req.body.password;
 
-//GET to new book page
-app.get("/new.ejs", async (req, res) => {
-  res.render("new.ejs",
-    { 
-      isbnError: invalidISBN,
+    try {
+      const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [ email ]);
+      if (checkResult.rows.length > 0 ) {
+        // user already exists
+        userAlreadyExist = "Email already in used, try logging in."
+        res.redirect("/signup");
+      } else {
+        // password hashing
+        bcrypt.hash(password, saltRounds, async (err, hash) => {
+          if (err) {
+            console.error("Error hashing password: ", err);
+          } else {
+            // insert user to the database
+            const result = await db.query(
+              "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *;",
+              [ email, hash]
+            );
+            const user = result.rows[0];
+            req.login(user, (err) => {
+              console.log("success");
+              res.redirect("/");
+            });
+          }
+        });
+      }
+    } catch (err) {
+      console.log(err);
     }
-  );
+});
+
+passport.use(
+  new Strategy(async function verify(username, password, cb) {
+    try {
+      const result = await db.query("SELECT * FROM users WHERE email = $1;", [ username ]);
+      if (result.rows.length > 0) {
+        const user = result.rows[0];
+        const storedHashedPassword = user.password;
+        bcrypt.compare(password, storedHashedPassword, (err, valid) => {
+          if (err) {
+            // Error with password check
+            console.error("Error comparing password: ", err);
+            return cb(err);
+          } else {
+            if (valid) {
+              // Passed password check
+              return cb(null, user);
+            } else {
+              // Did not pass password check
+              incorrectPsswrd = "The password you’ve entered is incorrect.";
+              return cb(null, false);
+            }
+          }
+        });
+      } else {
+        userNotFound = "The email you entered isn’t a registered account.";
+        return cb(null, false);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  })
+);
+
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+});
+passport.deserializeUser((user, cb) => {
+  cb(null, user);
 });
 
 app.listen(port, () => {
--- public/script.js
+++ public/script.js
@@ -3,6 +3,26 @@ let searchQuery = "";
 let sorted = "";
 let theGenre = "";
 
+// Check if password matches
+document.getElementById("registerForm").addEventListener("submit", function (event) {
+  const password = document.getElementById("password").value;
+  const confirmPassword = document.getElementById("confirmPassword").value;
+
+  if (password.length < 6) {
+    event.preventDefault(); // Prevent the form from submitting
+    document.getElementById("error-message").innerHTML = "Password must be at least 6 characters long.";
+    document.getElementById("error-message").style.display = "block";
+  } else {
+    if (password !== confirmPassword) {
+      event.preventDefault(); // Prevent the form from submitting
+      document.getElementById("error-message").innerHTML = "Passwords do not match!"
+      document.getElementById("error-message").style.display = "block";
+    } else {
+      document.getElementById("error-message").style.display = "none";
+    }
+  }
+});
+
 // SEARCH
 const input = document.getElementById('search-input');
 const bookshelf = document.getElementById('bookshelf');
